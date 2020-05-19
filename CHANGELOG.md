61bd3c0041b611f51706b3e6fc0ddb9a41a31e5f
HookyQR <hooky@qrmaster.co.nz>
2020-05-19 17:16:25 +0800

    move stack call to improve error output in specs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
-*** 0.0.5
+*** 0.0.7
+- Improve error stack generation
+
+*** 0.0.6
 - now exports a class which separates `root` changes from template calls
 - auto trim files by default (tail only)
 - add template locations to
--- ltsr.js
+++ ltsr.js
@@ -4,15 +4,14 @@ const storedFunctions = new Map();
 const primeRoot = process.cwd();
 const isString = val => Number.isNaN(parseInt(val));
 
-const REOriginalStack = Symbol('originalStack');
+const REOriginalError = Symbol('originalError');
 const RETemplates = Symbol('templates');
 const REBuiltStack = Symbol('builtStack');
 const RERoot = Symbol('root');
 
 class RenderError {
   constructor(referenceError, template, root) {
-    this[REOriginalStack] = referenceError.stack.replace('ReferenceError: ', '');
-    this.message = this[REOriginalStack].split('\n').shift();
+    this[REOriginalError] = referenceError;
     this[RERoot] = root;
     this[RETemplates] = [template];
   }
@@ -26,12 +25,15 @@ class RenderError {
     const target = /eval .*makeFunction.*<anonymous>(.*)\)/;
     return this[RETemplates].reduce(
       (stack, name) => stack.replace(target, `Template ${path.join(this[RERoot], name)}.lt$1`),
-      this[REOriginalStack]
-    ).replace(/\n\s*at LTSR.*/g, '');
+      this[REOriginalError].stack
+    ).replace(/\n\s*at LTSR.*/g, '').replace('ReferenceError: ', '');
   }
 
   get stack() {
     return this[REBuiltStack] || (this[REBuiltStack] = this.stackWithLt());
+  }
+  get message() {
+    return this.stack.split('\n').shift();
   }
 }
 
